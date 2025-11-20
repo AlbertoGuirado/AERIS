4b449bc0c9768a05e0debea0611207674a107f85
Alberto Guirado <alberto.guirado2000@gmail.com>
2025-11-20 23:25:04 +0100

    fixed backend

--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 # Instalar dependencias
-pip install -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
 
 # Ejecutar servidor
 uvicorn api:app --host=0.0.0.0 --port=8000
