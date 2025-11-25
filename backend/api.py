1df48c20245914e35b0f45103f6341c21b1a4b66
Alberto Guirado <alberto.guirado2000@gmail.com>
2025-11-25 13:10:24 +0100

    api fix

--- backend/api.py
+++ backend/api.py
@@ -23,7 +23,7 @@ app = FastAPI()
 # CORS para permitir React frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://aeris-proyect.vercel.app"],
+    allow_origins=["https://aeris-project.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
